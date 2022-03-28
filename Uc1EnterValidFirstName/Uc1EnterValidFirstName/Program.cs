d2a7bbba0150156d98afcb503d02603eb190f508
thakareshubh <shubhamthakare032@gmail.com>
2022-03-28 12:59:51 +0530

    Check first name

---
+++ Uc1EnterValidFirstName/Uc1EnterValidFirstName/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Uc1EnterValidFirstName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uc1 RegexProblem");
+            Console.WriteLine("Enter First name to check :");
+            Match("Shubham");
+        }
+         public  static void Match( string firstName)
+        {
+            if (Regex.Match(firstName, "^[A-Z][a-z]{3,}$").Success)
+            {
+                Console.WriteLine(" Firt Name is : " + firstName);
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+                throw new Exception();
+            }
+        }
+    }
+}
