d151ea86ce73439574016c13f22f4591e998868a
thakareshubh <shubhamthakare032@gmail.com>
2022-03-28 15:46:01 +0530

    Added password code to check

--- Uc1EnterValidFirstName/Uc1EnterValidFirstName/Program.cs
+++ Uc1EnterValidFirstName/Uc1EnterValidFirstName/Program.cs
@@ -7,19 +7,19 @@ namespace Uc1EnterValidFirstName
         static void Main(string[] args)
         {
             Console.WriteLine("RegexProblem");
-            Console.WriteLine("Enter Mobile number  to check :");
-            string mobileNumber=Console.ReadLine();
-            Match(mobileNumber);
+            Console.WriteLine("Enter Password  to check :");
+            string password=Console.ReadLine();
+            Match(password);
         }
         /// <summary>
-        /// Matches the specified mobile number.
+        /// Matches the specified password.
         /// </summary>
-        /// <param name="mobileNumber">The mobile number.</param>
-        public static void Match( string mobileNumber)
+        /// <param name="password">The password.</param>
+        public static void Match( string password)
         {
-            if (Regex.Match(mobileNumber, @"^([0-9]{2})\s([0-9]{10})$").Success)
+            if (Regex.Match(password, @"^([A-za-z]{5,})([@~!`#$%^&*]{1})([0-9]{1,})$").Success)
             {
-                Console.WriteLine(" Mobile Number is : " + mobileNumber);
+                Console.WriteLine("Success , Password is : " + password);
             }
             else
             {
